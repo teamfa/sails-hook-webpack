b0ed4e4ebc5b97944586fb8e09a7abab15f82fb9
Salakar <mike.diarmid@gmail.com>
2016-04-08 04:25:50 +0100

    v1.0.2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import webpack from 'webpack'
 
 export default function (sails) {
   const config = sails.config;
-
   const hook =  {
-
     configure: function () {
-      const config = sails.config;
       if (!config.webpack || !config.webpack.options) {
         sails.log.warn('sails-hook-webpack: no Webpack "options" are defined.');
         sails.log.warn('sails-hook-webpack: Please configure config/webpack.js')
@@ -18,20 +15,11 @@ export default function (sails) {
      * @param next
      */
     initialize: function (next) {
-      const config = sails.config;
       next();
-
-      if (!config.webpack || !config.webpack.options) {
-        return;
-      }
-
-      sails.after('lifted', () => {
-
-      })
     },
 
     /**
-     *
+     * Called after every webpack build.
      * @param err
      * @param rawStats
      * @returns {*}
@@ -41,10 +29,11 @@ export default function (sails) {
         return sails.log.error('sails-hook-webpack: Build error: \n\n', err);
       }
 
+      // emit a built event - hooks like sails-hook-react can then use this
+      // to reload sails routes in dev environment builds
       sails.emit('sails-hook-webpack:built', rawStats);
 
       const stats = rawStats.toJson();
-
       sails.log.debug('sails-hook-webpack: Build Info\n' + rawStats.toString({
           colors: true,
           chunks: false
@@ -53,27 +42,30 @@ export default function (sails) {
       if (stats.errors.length > 0) {
         sails.log.error('sails-hook-webpack: ', stats.errors)
       }
+
       if (stats.warnings.length > 0) {
         sails.log.warn('sails-hook-webpack: ', stats.warnings)
       }
     }
-
   };
 
-  hook.compiler = webpack(config.webpack.options, (err, stats) => {
-    if (err) throw err;
+  // setup outside like this to allow use of the compiler in http.customMiddleware
+  // configurations - i.e hot reload.
+  if (config.webpack && config.webpack.options) {
+    hook.compiler = webpack(config.webpack.options, (err, stats) => {
+      if (err) throw err;
+      sails.log.info('sails-hook-webpack: Webpack loaded.');
+      sails.log.silly('sails-hook-webpack: ', stats.toString());
 
-    sails.log.info('sails-hook-webpack: Webpack loaded.');
-    sails.log.silly('sails-hook-webpack: ', stats.toString());
-
-    if (process.env.NODE_ENV == 'development') {
-      sails.log.info('sails-hook-webpack: Watching for changes...');
-      hook.compiler.watch(config.webpack.watchOptions, hook.afterBuild)
-    } else {
-      sails.log.info('sails-hook-webpack: Running production build...');
-      hook.compiler.run(hook.afterBuild)
-    }
-  });
+      if (process.env.NODE_ENV == 'development') {
+        sails.log.info('sails-hook-webpack: Watching for changes...');
+        hook.compiler.watch(config.webpack.watchOptions, hook.afterBuild)
+      } else {
+        sails.log.info('sails-hook-webpack: Running production build...');
+        hook.compiler.run(hook.afterBuild)
+      }
+    });
+  }
 
   return hook;
 }
